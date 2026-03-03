f85d445798eafccd0e380f4f0745312b145187b6
hosamratoony-hue <hosamratoony@gmail.com>
2026-03-03 12:37:40 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,5 @@
-import "./globals.css"; // أضف هذا السطر في الأعلى
+import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ar">
-      <body>{children}</body>
-    </html>
-  );
-}
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar">
